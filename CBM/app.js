d00b96ef6850176baac11564967160e46768da5b
Gabriel <gabriel.maciasr@gmail.com>
2015-03-22 20:31:46 -0600

    REST

--- CBM/app.js
+++ CBM/app.js
@@ -1,5 +1,8 @@
 var express    = require("express");
 var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+
+
 var  connectionpool = mysql.createPool({
   host : 'localhost',
   user : 'root',
@@ -7,7 +10,8 @@ var  connectionpool = mysql.createPool({
   database : 'cbm'
 });
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/comics', function(req,res){
   connectionpool.getConnection(function(err, connection) {
@@ -20,7 +24,7 @@ app.get('/comics', function(req,res){
       });
     } 
     else {
-      connection.query('SELECT * FROM test', req.params.id, function(err, rows, fields) {
+      connection.query('Select comics.issue, series.name, series.publisher,comics.release_date  from comics join series on comics.series_id=series.idseries',  function(err, rows, fields) {
         if (err) {
           console.error(err);
           res.statusCode = 500;
@@ -35,5 +39,155 @@ app.get('/comics', function(req,res){
     }
   });
 });
+
+app.post('/add_user', function(req,res){
+
+  var data={
+    mail:req.body.mail, 
+    password: req.body.password
+  };
+  connectionpool.getConnection(function(err, connection) {
+
+    if (err) {
+      console.error('CONNECTION error: ',err);
+      res.statusCode = 503;
+      res.send({
+        result: 'error',
+        err: err.code
+      });
+    } 
+    else {
+      connection.query('INSERT into user set ?', data, function(err, rows, fields) {
+        if (err) {
+          console.error(err);
+          res.statusCode = 500;
+          res.send({
+            result: 'error',
+            err: err.code
+          });
+        }
+        res.send(rows);
+        connection.release();
+      });
+    }
+  });
+});
+
+app.post('/add_comic', function(req,res){
+  var data;
+  var userid;
+
+  connectionpool.getConnection(function(err, connection) {
+    connection.query('SELECT iduser from user where mail = ?', req.body.mail, function(err, rows, fields) {
+      userid=rows[0].iduser;
+      data={
+        user_id: userid, 
+        comic: req.body.comicid
+      };
+      console.log(data);
+    });
+    connection.query('INSERT into user_has_comis set ? ', data, function(err, rows, fields) {
+        if (err) {
+          console.error(err);
+          res.statusCode = 500;
+          res.send({
+            result: 'error',
+            err: err.code
+          });
+        }
+        res.send(rows);
+        connection.release();
+      });
+  });
+});
+app.delete('/delete_user', function(req,res){
+
+  connectionpool.getConnection(function(err, connection) {
+
+    if (err) {
+      console.error('CONNECTION error: ',err);
+      res.statusCode = 503;
+      res.send({
+        result: 'error',
+        err: err.code
+      });
+    } 
+    else {
+      connection.query('DELETE from user where mail = ?', req.body.mail, function(err, rows, fields) {
+        if (err) {
+          console.error(err);
+          res.statusCode = 500;
+          res.send({
+            result: 'error',
+            err: err.code
+          });
+        }
+        res.send(rows);
+        connection.release();
+      });
+    }
+  });
+});
+
+app.put('/update_password/', function(req,res){
+
+  connectionpool.getConnection(function(err, connection) {
+
+    if (err) {
+      console.error('CONNECTION error: ',err);
+      res.statusCode = 503;
+      res.send({
+        result: 'error',
+        err: err.code
+      });
+    } 
+    else {
+      connection.query('UPDATE user set password = ? where mail = ?', [req.body.password,req.body.mail], function(err, rows, fields) {
+        if (err) {
+          console.error(err);
+          res.statusCode = 500;
+          res.send({
+            result: 'error',
+            err: err.code
+          });
+        }
+        res.send(rows);
+        connection.release();
+      });
+    }
+  });
+});
+
+app.put('/update_mail/', function(req,res){
+
+  connectionpool.getConnection(function(err, connection) {
+
+    if (err) {
+      console.error('CONNECTION error: ',err);
+      res.statusCode = 503;
+      res.send({
+        result: 'error',
+        err: err.code
+      });
+    } 
+    else {
+      console.log(req.body.nmail);
+
+      connection.query('UPDATE user set mail = ? where mail = ?', [req.body.nmail,req.body.omail], function(err, rows, fields) {
+        if (err) {
+          console.error(err);
+          res.statusCode = 500;
+          res.send({
+            result: 'error',
+            err: err.code
+          });
+        }
+        res.send(rows);
+        connection.release();
+      });
+    }
+  });
+});
+
 
 app.listen(3000);
